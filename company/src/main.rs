9de15758db71f0bc359bffbcf48ea1de5249b43c
etch1000 <hmoroliya@ariqt.com>
2022-11-02 10:35:49 +0530

    Edited: Company main.rs changes based on Cargo Clippy

--- company/src/main.rs
+++ company/src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::{self, BufRead}, collections::HashMap};
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
 
 fn main() {
     let mut employees: HashMap<String, Vec<String>> = HashMap::new();
@@ -17,7 +20,7 @@ fn main() {
         let input = line.expect("error: unable to read user input");
 
         match Command::from_input(&input) {
-            Some(Command::Add {dept, name}) => employees.entry(dept).or_default().push(name),
+            Some(Command::Add { dept, name }) => employees.entry(dept).or_default().push(name),
             Some(Command::List(dept)) => match employees.get(&dept) {
                 Some(names) => {
                     for name in names {
@@ -36,12 +39,12 @@ fn main() {
                         println!("{}: {}", dept, name);
                     }
                 }
-            },
+            }
             Some(Command::Quit) => break,
             None => println!("Input error"),
         }
     }
-    
+
     println!("Have a nice day!");
 }
 
@@ -54,16 +57,16 @@ enum Command {
 
 impl Command {
     fn from_input(s: &str) -> Option<Self> {
-        let words: Vec<&str> = s.trim().split_whitespace().collect();
+        let words: Vec<&str> = s.split_whitespace().collect();
 
         match words.as_slice() {
             ["All"] => Some(Command::All),
             ["Quit"] => Some(Command::Quit),
             ["List", dept] => Some(Command::List(String::from(*dept))),
             ["Add", name, "to", dept] => Some(Command::Add {
-                    dept: String::from(*dept),
-                    name: String::from(*name),
-                }),
+                dept: String::from(*dept),
+                name: String::from(*name),
+            }),
             _ => None,
         }
     }
